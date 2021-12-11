00c16f50665854a794eb582db2a9f1692657de89
matthewshort-dev <matthewshort.dev@gmail.com>
2021-12-10 23:08:43 -0500

    Initial Project Setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Pickaxe2.0 link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Pickaxe2.0/i);
+  expect(element).toBeInTheDocument();
 });
